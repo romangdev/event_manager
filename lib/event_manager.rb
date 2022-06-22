71355202cb74fbbec9203f2095d191aa54f7db8c
Roman Geathers <romangeathersdev@gmail.com>
2022-06-22 13:47:54 -0400

    Clean registrant phone numbers
    Add method that cleans all registrant phone numbers into one, easily
    readable and similar format. Return all zeros for bad numbers.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,43 +1,69 @@
-require "csv"
-require "google/apis/civicinfo_v2"
-require "erb"
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
 
-def clean_zipcode(zipcode) 
-  zipcode.to_s.rjust(5, "0")[0..4]
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
 end
 
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-  civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
     civic_info.representative_info_by_address(
       address: zip,
-      levels: "country",
-      roles: ["legislatorUpperBody", "legislatorLowerBody"]
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
     ).officials
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
-def save_thank_you_letter(id, form_letter)
-  Dir.mkdir("output") unless Dir.exist?("output")
-  filename = "output/thanks_#{id}.html"
+def clean_phone_number(phone)
+  number = phone.split("")
+  for i in 0..1
+    number.each do |char|
+      number.delete(char) if char != "0" && char.to_i == 0
+    end
+  end
 
-  File.open(filename, "w") do |file|
-    file.puts form_letter
+  if number.length > 10 && number[0] == "1"
+    number.shift
+  elsif number.length > 10 || number.length < 10
+    number = "0000000000"
+  else
+    number
+  end
+
+  begin
+    number = number.join.to_i
+  rescue
+    number
+  end
+  number
 end
 
-puts "Event Manager Initialized!"
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts 'EventManager initialized.'
 
 contents = CSV.open(
-  "event_attendees.csv", 
-  headers: true, 
+  'event_attendees.csv',
+  headers: true,
   header_converters: :symbol
 )
 
-template_letter = File.read("form_letter.erb")
+template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
 contents.each do |row|
@@ -45,8 +71,8 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
+  phone = clean_phone_number(row[:homephone])
 
   form_letter = erb_template.result(binding)
-  save_thank_you_letter(id, form_letter)
-  end
+  save_thank_you_letter(id,form_letter)
 end
